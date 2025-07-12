d94a796af63ea13e9bb8d4968382e90a8589842d
Kim Taeyeong <xodud7737@gmail.com>
2025-07-12 11:55:26 +0900

    _

--- app/src/main/java/com/example/librarytimer/ui/theme/Color.kt
+++ app/src/main/java/com/example/librarytimer/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackGroundColor = Color(65,61,85)
+val OnColor = Color(47,238,60)
+val OffColor = Color(238,50,47)
+val SaveButtonColor = Color(68,95,219)
+val AllIconColor = Color.White
+
+val TextColor = Color.White
+
+val TimeSetButtonColor = Color(217,217,217)
+val TimerBackgroundColor = Color(39,33,33)
+
